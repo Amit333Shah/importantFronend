4a01b72056f3b2af3c1ffa8752e267f43950cba0
Amit Shah <72549583+Amit333Shah@users.noreply.github.com>
2024-07-15 00:56:00 +0530

    Update unique_number.blade.php

--- unique_number.blade.php
+++ unique_number.blade.php
@@ -1,13 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Unique Number</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+input[type=email], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
 </head>
 <body>
-    <h1>Hello,</h1>
-    <p>Here is your unique 6-digit number: <strong>{{ $uniqueNumber }}</strong></p>
-    <p>Thank you for your attention.</p>
+
+<h3>Contact Form</h3>
+
+<div class="container">
+  <form action="{{route('emailPost')}}" method="POST">
+    @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+    @csrf
+    <label for="name">First Name</label>
+    <input type="text" id="fname" name="name" placeholder="Your name..">
+
+    <label for="email">Email</label>
+    <input type="email" id="lname" name="email" placeholder="Your la..">
+    <label for="phone">Phone Number</label>
+    <input type="text" id="fname" name="phone" placeholder="phone">
+
+
+    <label for="country">Country</label>
+    <select id="department_id" name="department_id">
+      <option value="1">Australia</option>
+      <option value="2">Canada</option>
+      
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
+</div>
+
 </body>
 </html>
